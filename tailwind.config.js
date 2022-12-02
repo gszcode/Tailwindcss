8358204e58eaf4657df0b55add65bbf2bf26b4a4
Gabriel <gszcode@gmail.com>
2022-12-01 22:26:33 -0300

    proyecto-practica de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./public/**/*.html'],
+  media: false,
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        'wave-pattern': 'url(../img/wave-white.png)',
+        'wave-pattern-dots': 'url(../img/pattern-white-dots.png)'
+      })
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
