2bb090acfc8452edaba65fb7e78fb9d0aed13b77
Gabriel <gszcode@gmail.com>
2022-12-02 01:37:08 -0300

    :P

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,15 @@ module.exports = {
     extend: {
       backgroundImage: (theme) => ({
         'wave-pattern': 'url(../img/wave-white.png)',
-        'wave-pattern-dots': 'url(../img/pattern-white-dots.png)'
-      })
+        'wave-pattern-dots': 'url(../img/pattern-white-dots.png)',
+        'pattern-character-red': 'url(../img/pattern-characters-red.png)',
+        'character-mario': 'url(../img/character-l.png)',
+        'wave-pink': 'url(../img/wave-pink.png)',
+        'yellow-dots': 'url(../img/pattern-yellow-dots.png)'
+      }),
+      screens: {
+        xsm: '320px'
+      }
     }
   },
   variants: {
